7efef894f761cfc0c0e39b2dd6b2737f5e40e50f
OLIVIASUN96 <15301048@bjtu.edu.cn>
2019-11-16 14:03:56 -0500

    remove outliers; shuffle data before save; seperate embedding with other infomarion; covert cluster label into onehot;

--- models/random_forest_model.py
+++ models/random_forest_model.py
@@ -1,18 +1,17 @@
+
 """
 Random Forest Model
 
 """
-import os, sys
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV,train_test_split,cross_val_score, cross_val_predict
+import pre_data
+from basic_regressor import Basic_regressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
-
-# import local
-root_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, root_path)
-import pre_data
-from models.basic_regressor import Basic_regressor
+from utilities import evaluation
+import matplotlib.pyplot as plt
 
 class Randomforest(Basic_regressor):
     def __init__(self, config=None, exp_name='new_exp',rfr = None):
@@ -20,27 +19,76 @@ class Randomforest(Basic_regressor):
         self.exp_name = exp_name
         self.rfr = RandomForestRegressor()
 
+    def train(self, features, response,columns):
+        params = {}
+        # default para
+        self.rfr = RandomForestRegressor(
+            oob_score=True,
+            random_state=10)
+        self.rfr.fit(features,response)
+        print("oob_score", self.rfr.oob_score_)
 
-    def train(self, features, response):
         # Perform Grid-Search
+        param_test1 = {'n_estimators': (10, 50, 100, 1000)}
         gsc = GridSearchCV(
             estimator=RandomForestRegressor(),
-            param_grid={
-                'max_depth': range(3, 7),
-                'n_estimators': (10, 50, 100, 1000),
-            },
+            param_grid=param_test1,
             cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1
         )
         grid_result = gsc.fit(features, response)
-        best_params = grid_result.best_params_
-        self.rfr = RandomForestRegressor(max_depth=best_params["max_depth"],
-                                                 n_estimators=best_params["n_estimators"],
+        print(grid_result.grid_scores_,grid_result.best_params_,grid_result.best_score_)
+        params["n_estimators"] = grid_result.best_params_["n_estimators"]
+
+        param_test2 = {'max_depth':range(3,14,2), 'min_samples_split':range(50,201,20)}
+        gsc = GridSearchCV(
+            estimator=RandomForestRegressor(),
+            param_grid=param_test2,
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1
+        )
+        grid_result = gsc.fit(features, response)
+        print(grid_result.grid_scores_, grid_result.best_params_, grid_result.best_score_)
+        params["max_depth"] = grid_result.best_params_["max_depth"]
+        params["min_samples_split"] = grid_result.best_params_["max_depth"]
+
+
+        param_test3 = { 'min_samples_leaf': range(10, 60, 10)}
+        gsc = GridSearchCV(
+            estimator=RandomForestRegressor(),
+            param_grid=param_test3,
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1
+        )
+        grid_result = gsc.fit(features, response)
+        print(grid_result.grid_scores_, grid_result.best_params_, grid_result.best_score_)
+        params["min_samples_leaf"] = grid_result.best_params_["min_samples_leaf"]
+
+        self.rfr = RandomForestRegressor( n_estimators=params["n_estimators"],
+                                                 max_depth = params["max_depth"],
+                                                 min_samples_split = params["min_samples_split"],
+                                                 min_samples_leaf = params["min_samples_leaf"],
+                                                 oob_score = True,
                                                  random_state=False, verbose=False)
+
         self.rfr.fit(features,response)
+
+        # see the importance of features
+        print(columns)
+        importances = self.rfr.feature_importances_
+        indices = np.argsort(importances)[::-1]
+        for f in range(features.shape[1]):
+            print(str(f + 1), columns[indices[f]],importances[indices[f]])
+
+        data = importances
+        labels = columns
+
+        plt.bar(range(len(data)), data, tick_label=labels)
+        plt.show()
+
+
+        print("oob_score", self.rfr.oob_score_)
+
 
 
     def predict(self,features,response):
-
         predictions = self.rfr.predict(features)
         print("predictions",predictions)
         # print("response",test_score)
@@ -50,17 +98,36 @@ class Randomforest(Basic_regressor):
         MSE = mean_squared_error(predictions, response);
         #print("test_score",test_score)
         print("MSE",MSE)
-        #print(predictions)
-        return predictions
+
+    def evaluation(self,housedata):
+        """
+                Input: housedata: instance of class pre_data.Housedata
+                """
+        k = 9
+        performance = []
+        for i in range(k):
+            self.train(housedata.train_features[i], housedata.train_targets[i])
+            predictions = self.predict(housedata.valid_features[i])
+            targets = housedata.valid_targets[i]
+            performance.append(evaluation(predictions, targets))
+        print(performance)
+
+
 
 
 if __name__ == "__main__":
     data_HOA,data_LOT = pre_data.pre_data()
-    data = train_test_split(data_HOA.data, data_HOA.target,
-                            test_size=0.7, random_state=0)
-    X_train, X_test, y_train, y_test = data
+    otherinfo = data_HOA.otherinfo
+    print(otherinfo)
+    X_train = data_HOA.train_features
+    y_train = data_HOA.train_targets
+
+    columns = data_HOA.data.columns
+    # data = train_test_split(data_HOA.data, data_HOA.target,
+    #                         train_size=0.7, random_state=0)
+    # X_train, X_test, y_train, y_test = data
 
     rfr_model = Randomforest()
-    rfr_model.train(X_train,y_train)
-    rfr_model.predict(X_test,y_test)
+    rfr_model.train(X_train[0],y_train[0],columns)
+    #rfr_model.predict(X_test,y_test)
 
