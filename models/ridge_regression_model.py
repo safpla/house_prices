47ae5c726783205b5a7dab838b1735267ba53cb9
Dorris <dorriszh@gmail.com>
2019-11-16 00:45:38 -0500

    revise import

--- models/ridge_regression_model.py
+++ models/ridge_regression_model.py
@@ -5,7 +5,6 @@ from basic_regressor import Basic_regressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
-import Ipynb_importer
 import pre_data
 
 class grid():
